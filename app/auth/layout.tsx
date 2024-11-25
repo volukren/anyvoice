92b3582ef8ec5f162e44f82028a637802d8ad49f
nurbek <volukren@gmail.com>
2024-11-25 22:56:55 +0600

    feat: logo in signin page

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function AuthLayout({
   children,
@@ -8,7 +9,11 @@ export default function AuthLayout({
   return (
     <main className="h-screen w-full flex justify-center items-center bg-gray-100">
       <div className="w-96 rounded-md p-5 bg-gray-50 flex flex-col gap-5">
-        <Link href="/" className="font-bold text-xl lg:text-xl text-center">
+        <Link
+          href="/"
+          className="font-bold text-xl lg:text-xl text-center flex items-center gap-1 mx-auto"
+        >
+          <Image src="/logo.png" alt="AnyVoice logo" width={32} height={32} />
           <h1>AnyVoice</h1>
         </Link>
         {children}
