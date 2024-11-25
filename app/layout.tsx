98ad6fc6ec45dc7cc0baf5c567e4f5ad4840e242
nurbek <volukren@gmail.com>
2024-11-25 23:47:10 +0600

    feat: plausible

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Providers from "@/app/providers";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth/options";
 import Script from "next/script";
+import PlausibleProvider from "next-plausible";
 
 const inter = Inter({
   weight: ["400", "500", "600", "700", "800", "900"],
@@ -29,9 +30,12 @@ export default async function RootLayout({
       <body
         className={`${inter.className} antialiased text-sm text-foreground`}
       >
-        <Providers session={session}>{children}</Providers>
+        <Providers session={session}>
+          <PlausibleProvider domain="anyvoice.app">
+            {children}
+          </PlausibleProvider>
+        </Providers>
       </body>
-      <Script async src="https://scripts.simpleanalyticscdn.com/latest.js" />
     </html>
   );
 }
