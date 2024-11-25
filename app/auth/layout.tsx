56bd84bca6104a9c4c17221d2ad9b434a280105e
nurbek <volukren@gmail.com>
2024-11-25 21:58:35 +0600

    fix: no h1 error fix

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -9,7 +9,7 @@ export default function AuthLayout({
     <main className="h-screen w-full flex justify-center items-center bg-gray-100">
       <div className="w-96 rounded-md p-5 bg-gray-50 flex flex-col gap-5">
         <Link href="/" className="font-bold text-xl lg:text-xl text-center">
-          AnyVoice
+          <h1>AnyVoice</h1>
         </Link>
         {children}
       </div>
