ddcb8a529c2d63e342a299989af47263b89c3136
nurbek <volukren@gmail.com>
2024-12-08 21:45:25 +0600

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,6 +6,9 @@ export default function Footer() {
       <div className="max-w-5xl mx-auto flex flex-col md:flex-row justify-start md:justify-between md:items-start">
         <div className="text-sm text-left md:text-center">
           &copy; {new Date().getFullYear()} AnyVoice. All rights reserved.{" "}
+          <div className="flex flex-col gap-2 text-left mt-4">
+            <a href="https://sprunki-mod.com/">Sprunki Mod</a>
+          </div>
         </div>
         <div className="flex flex-col md:flex-row pt-4 md:pt-0 md:justify-end md:items-center gap-4">
           <Link href="/privacy-policy" className="underline">
