2990df8136afa84c6bada15bc030577561b06cd4
nurbek <volukren@gmail.com>
2025-01-15 04:24:57 +0600

    added link to pmjobs

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,13 +7,22 @@ export default function Footer() {
         <div className="text-sm text-left md:text-center">
           &copy; {new Date().getFullYear()} AnyVoice. All rights reserved.
         </div>
-        <div className="flex flex-col md:flex-row pt-4 md:pt-0 md:justify-end md:items-center gap-4">
-          <Link href="/privacy-policy" className="underline">
-            Privacy Policy
-          </Link>
-          <Link href="/tos" className="underline">
-            Terms of Service
-          </Link>
+        <div className="grid grid-cols-1 md:grid-cols-2 mt-5 md:mt-0 gap-5">
+          <div className="flex flex-col gap-2">
+            <div className="font-bold text-black/70">Links</div>
+            <Link href="https://allpmjobs.com" className="underline">
+              AllPMJobs
+            </Link>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="font-bold text-black/70">Resources</div>
+            <Link href="/privacy-policy" className="underline">
+              Privacy Policy
+            </Link>
+            <Link href="/tos" className="underline">
+              Terms of Service
+            </Link>
+          </div>
         </div>
       </div>
     </div>
