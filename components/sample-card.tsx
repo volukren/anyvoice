88a2a21c60aea673bfcf3de7dfbf8c24a50e2e2c
nurbek <volukren@gmail.com>
2025-02-06 17:49:33 +0600

    hide delete button for public samples

--- components/sample-card.tsx
+++ components/sample-card.tsx
@@ -8,6 +8,7 @@ type SampleCardProps = {
   id: number;
   name: string;
   createdAt: Date;
+  public: boolean;
 };
 
 export default function SampleCard({ sample }: { sample: SampleCardProps }) {
@@ -19,7 +20,7 @@ export default function SampleCard({ sample }: { sample: SampleCardProps }) {
         "flex flex-col gap-4 bg-base-100 border rounded-md text-center p-3 relative"
       )}
     >
-      <DeleteSampleButton id={sample.id} />
+      {!sample.public &&  <DeleteSampleButton id={sample.id} />}
       <span className="font-bold text-lg">{sample.name}</span>
       <span>{dayjs(sample.createdAt).format("MMM D, YYYY")}</span>
     </Link>
